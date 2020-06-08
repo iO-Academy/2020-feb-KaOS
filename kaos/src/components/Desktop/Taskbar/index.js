f752a1925b57e1ffbf13d93cc7bba7e164a20a7a
Vinc <vinc_944@hotmail.com>
2020-06-08 10:39:40 +0100

    Made icon Component

--- kaos/src/components/Desktop/Taskbar/index.js
+++ kaos/src/components/Desktop/Taskbar/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import './TaskBar.css';
 import Clock from '../../Clock'
+import IconComponent from "./Icon";
 
 export default class TaskBar extends React.Component {
     render() {
         return (
             <div className='TaskBar'>
+                <div><IconComponent name='About' open={this.props.toggleApplication}/></div>
                 <Clock />
             </div>
         );
